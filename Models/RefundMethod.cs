e7d1d87b4610cd929daccada2dead553e50d7bba
ArshiaShabazpour <arshiashahbazpour@gmail.com>
2025-10-28 22:17:55 -0400

    created database entities, context and a migration

---
+++ Models/RefundMethod.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+namespace ECommerceApp.Models
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum RefundMethod
+    {
+        Original,   
+        PayPal,
+        Stripe,
+        BankTransfer,
+        Manual
+    }
+}
